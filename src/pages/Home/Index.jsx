72ac9a9d7ac7c540ad92d05083208b8dc4f800a7
jorge uribe <jouribero@cesde.net>
2024-12-20 16:54:28 -0500

    imagen-card

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then((response) => response.json())
+      .then(data => setItems(data))
+  }, []);
+
   return (
     <Layout>
-      <div>
-        Home
-        <Card />
-      </div>
+      Home
+      {
+        items?.map(items => (
+          <Card key={items.id} data={items}/> 
+
+        ))
+      }
     </Layout>
   );
 }
 
 export default Home;
-
-
